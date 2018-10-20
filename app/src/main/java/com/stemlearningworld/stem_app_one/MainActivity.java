79786432aace9d81f09372505cc2f18e05a67cc1
donaldBQ <donaldbq@gmail.com>
2018-10-20 17:18:37 -0600

    Activity Main UI lista

--- app/src/main/java/com/stemlearningworld/stem_app_one/MainActivity.java
+++ app/src/main/java/com/stemlearningworld/stem_app_one/MainActivity.java
@@ -2,6 +2,7 @@ package com.stemlearningworld.stem_app_one;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button Reading= (Button) findViewById(R.id.Reading_Button);
+        final Button Recording= (Button) findViewById(R.id.Recording_Button);
+        final Button Audio= (Button) findViewById(R.id.Audio_Button);
+        final Button Test= (Button) findViewById(R.id.Test_Button);
+        final Button Video= (Button) findViewById(R.id.Files_Button);
     }
 }
