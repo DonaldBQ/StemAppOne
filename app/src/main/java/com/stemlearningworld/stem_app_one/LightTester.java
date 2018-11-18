c335afd46f72b008508e05719f346c031d594403
donaldBQ <donaldbq@gmail.com>
2018-11-18 16:48:51 -0600

    Light Sensor OK

---
+++ app/src/main/java/com/stemlearningworld/stem_app_one/LightTester.java
@@ -0,0 +1,161 @@
+package com.stemlearningworld.stem_app_one;
+
+import android.app.Service;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import java.util.ArrayList;
+
+public class LightTester extends AppCompatActivity implements SensorEventListener{
+
+    TextView textView;
+    SensorManager sensorManager;
+    Sensor sensor;
+    static double lightInput;
+
+    HorizontalBarChart LightChart;
+    int optimalLux = 700;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smart);
+
+        textView = (TextView) findViewById(R.id.textView);
+        sensorManager = (SensorManager)getSystemService(Service.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        LightChart = (HorizontalBarChart) findViewById(R.id.chart1);
+
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        horizontalChart((float) lightInput);
+
+                    }
+                });
+                try {
+                    Thread.sleep(600);
+                } catch (InterruptedException e) {
+
+                }
+            }
+        }).start();
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType()==Sensor.TYPE_LIGHT){
+            lightInput = event.values[0];
+            textView.setText("" + String.format("%.2f", lightInput));
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+    private void horizontalChart(float value){
+        String[] labels = {
+                "Light",
+                "Noise"
+        };
+
+        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
+        BarEntry EntriesLight = new BarEntry(0, (float) value); // Light value
+        valueSet1.add(EntriesLight);
+
+        ArrayList<BarEntry> valueSet2 = new ArrayList<>();
+        BarEntry EntriesNoise = new BarEntry(0,optimalLux ); // Light value
+        valueSet2.add(EntriesNoise);
+
+
+        BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Cantidad de luz actual");
+
+        barDataSet1.setColors(new int[] { R.color.accent }, this);
+
+        BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Cantidad de luz óptima");
+
+        barDataSet2.setColors(new int[] { R.color.audiocolor}, this);
+
+//        BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Brand 2");
+//        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
+
+        if(lightInput>optimalLux){
+            dataSets.add(barDataSet1);
+            dataSets.add(barDataSet2);
+        }
+        else if(lightInput<optimalLux){
+            dataSets.add(barDataSet2);
+            dataSets.add(barDataSet1);
+        }else {
+
+                dataSets.add(barDataSet1);
+                dataSets.add(barDataSet2);
+
+        }
+
+
+        BarData GoalsBardata = new BarData(dataSets);
+        HorizontalBarChart chart = (HorizontalBarChart) findViewById(R.id.chart1);
+        chart.setData(GoalsBardata);
+        chart.animateXY(5000, 5000);
+        chart.setDrawGridBackground(true);
+        chart.setDrawValueAboveBar(true);
+        chart.setBackgroundColor(Color.WHITE);
+        chart.getXAxis().setValueFormatter(new LabelFormatter(labels));
+        chart.getAxisRight().setDrawLabels(false);
+        chart.getXAxis().setDrawLabels(false);
+        chart.getAxisLeft().setAxisMinimum(0);
+        chart.getAxisRight().setAxisMinimum((float) (lightInput + lightInput * 0.2));
+        chart.notifyDataSetChanged();
+        chart.invalidate();
+    }
+    public class LabelFormatter implements IAxisValueFormatter {
+        private final String[] mLabels;
+
+        public LabelFormatter(String[] labels) {
+            mLabels = labels;
+        }
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            return mLabels[(int) value];
+        }
+    }
+
+
+}
--- app/src/main/java/com/stemlearningworld/stem_app_one/MainActivity.java
+++ app/src/main/java/com/stemlearningworld/stem_app_one/MainActivity.java
@@ -1,17 +1,34 @@
 package com.stemlearningworld.stem_app_one;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
 
+
+    private final static int Interval = 1000*30*1; // 2 minutos
+    Handler mHandler = new Handler();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +49,10 @@ public class MainActivity extends AppCompatActivity {
         //Retrieve an instance of the database and reference the location you want to read
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReferenceFromUrl("https://stemappone.firebaseio.com/");
+
+        //Plotting information
+
+
 
         //********Begin: Getting Linear Layouts and implementing them on click listener********
         //Button Student Book
@@ -110,7 +136,42 @@ public class MainActivity extends AppCompatActivity {
                startActivity(intent);
             }
         });
+        //Button Smart
+        LinearLayout SmartLayout = (LinearLayout)findViewById(R.id.smart);
+        SmartLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //setContentView(R.layout.activity_smart);
+               // mHandlerTask.run();
+
+               startActivity(new Intent(MainActivity.this, LightTester.class));
+
+
+            }
+        });
     //********End: Getting Linear Layouts and implementing them on click listener*******
     }
+    //***********Begin: Graficacion**********************
+
+/*        Runnable mHandlerTask = new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, LightTester.class));
+                mHandler.postDelayed(mHandlerTask, Interval);
+            }
+        };
+        void startRepetingTask(){
+            mHandlerTask.run();
+        }
+        void stopRepeatingTask(){
+            mHandler.removeCallbacks(mHandlerTask);
+        }*/
+
+
+
+
+
+
+    //*****************End: Graficacion******************
 
 }
